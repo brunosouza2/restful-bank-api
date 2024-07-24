e1c31cc1e87d454d96ea81d492471a81e31766ba
Bruno Souza <bruno.henrqsouza@gmail.com>
2024-07-24 13:42:46 -0300

    hotfix: changing scale and precision of additional limit and available limit

--- src/main/java/me/com/bank/domain/model/Card.java
+++ src/main/java/me/com/bank/domain/model/Card.java
@@ -15,7 +15,7 @@ public class Card {
 
     private String number;
 
-    @Column(name = "available_limit", scale = 13, precision = 2)
+    @Column(name = "available_limit", scale = 2, precision = 13)
     private BigDecimal limit;
 
     public Card(Long id, String number, BigDecimal limit) {
